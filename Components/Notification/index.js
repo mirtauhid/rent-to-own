9aedcf0d9459f1f1348091514dd0b5391b72d3d7
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2021-09-09 09:26:37 +0600

    signup modal form validation completed

--- Components/Notification/index.js
+++ Components/Notification/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import SignUpModal from '../Modal/SignUpModal';
 
 const Notification = () => {
     return (
         <div>
-            <SignUpModal isOpen={true}/>
-            <div className="w-1/2 mx-auto my-24 text-center">
-                <h2 className="text-red-500 font-bold text-4xl">WE ARE SORRY THAT YOU DON'T MEET THE MINIMUM REQUIREMENTS AT THIS TIME.</h2>
-                <p className="text-xl mt-5">However, we realize things may change for you down the road.  When you do meet our minimum requirements, please complete another application at that time.</p>
+            <div className="lg:w-1/2 md:w-2/3 mx-auto my-24 text-center px-4">
+                <h2 className="text-red-500 font-bold text-2xl lg:text-4xl md:text-3xl">WE ARE SORRY THAT YOU DON'T MEET THE MINIMUM REQUIREMENTS AT THIS TIME.</h2>
+                <p className="lg:text-xl md:text-lg text-md mt-5">However, we realize things may change for you down the road.  When you do meet our minimum requirements, please complete another application at that time.</p>
             </div>
         </div>
     );
