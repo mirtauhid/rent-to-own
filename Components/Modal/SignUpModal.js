53c68b7fcebbdbc5a3c07162c617d80ac43cef40
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2021-09-09 08:16:49 +0600

    modified signup modal

--- Components/Modal/SignUpModal.js
+++ Components/Modal/SignUpModal.js
@@ -29,13 +29,14 @@ const customStyles = {
 const SignUpModal = ({isOpen}) => {
 
     const validate = values => {
+        const errors = {};
+        
         if (!values.email) {
             errors.email = 'Required';
         } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
             errors.email = 'Invalid email address';
         }
 
-        const errors = {};
         if (!values.firstName) {
             errors.firstName = 'First name required';
         }
@@ -81,7 +82,7 @@ const SignUpModal = ({isOpen}) => {
                                 id="email"
                                 type="text"
                                 placeholder="Email"
-                                // onChange={formik.handleChange}
+                                onChange={formik.handleChange}
                                  />
                                 {
                                     formik.errors.email &&
--- Components/Notification/index.js
+++ Components/Notification/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import SignUpModal from '../Modal/SignUpModal';
 
 const Notification = () => {
     return (
         <div>
+            <SignUpModal isOpen={true}/>
             <div className="w-1/2 mx-auto my-24 text-center">
                 <h2 className="text-red-500 font-bold text-4xl">WE ARE SORRY THAT YOU DON'T MEET THE MINIMUM REQUIREMENTS AT THIS TIME.</h2>
                 <p className="text-xl mt-5">However, we realize things may change for you down the road.  When you do meet our minimum requirements, please complete another application at that time.</p>
