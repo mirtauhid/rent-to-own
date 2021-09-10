a9c843e4f72e7ee819e4249c00212bd937cdec4a
Imran Hossain <imran@algosolver.com>
2021-09-10 11:29:38 +0600

    adding footer

---
+++ Layouts/HomeLayout/Footer/index.js
@@ -0,0 +1,12 @@
+const Footer = () => (
+    <div className="container mx-auto">
+        <div className="grid grid-cols-12">
+            <div className="col-span-4">
+
+            </div>
+        </div>
+    </div>
+)
+
+
+export default Footer
