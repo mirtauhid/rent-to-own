05f3c15918a9d1c6253a4ed9903dcb23eac0cbbf
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2021-09-15 11:31:15 +0600

    changed photos functionality

--- Components/SubPage/ListPropertyPages/Photos.js
+++ Components/SubPage/ListPropertyPages/Photos.js
@@ -1,27 +1,9 @@
-import { useFormik } from 'formik';
 import React, { useState } from 'react';
-import { FaCloudUploadAlt } from 'react-icons/fa';
+import { FaCloudUploadAlt, FaTimes } from 'react-icons/fa';
 
 const Photos = ({ steps, setSteps }) => {
-    const [photo, setPhoto] = useState([])
-    const validate = values => {
-        const errors = {};
-
-        if (!values.upload) { errors.upload = 'Required' }
-
-        return errors;
-    };
-
-    const formik = useFormik({
-        initialValues: {
-            upload: '',
-        },
-        validate,
-        onSubmit: values => {
-            console.log(values);
-            setSteps({ ...steps, fourth: true })
-        },
-    });
+    const [photos, setPhotos] = useState([])
+    const [files, setFiles] = useState([])
 
     const convertBase64 = (file) => {
         return new Promise((resolve, reject) => {
@@ -45,17 +27,38 @@ const Photos = ({ steps, setSteps }) => {
             return;
         }
         console.log(base64)
-        setPhoto([...photo, base64])
+        setPhotos([...photos, base64])
+        setFiles([...files, e.target.files[0]])
     };
+
+    const handleDeleteImg = (index) => {
+
+        setPhotos(photos.filter((photo, i) => i != index))
+        setFiles(photos.filter((photo, i) => i != index))
+    }
     return (
         <div className="p-6">
             <h2 className="uppercase text-center text-2xl font-bold my-5">ADD A FEW PHOTOS</h2>
-            <form onSubmit={formik.handleSubmit} >
-
-
-                <div className="flex w-full mb-2 p-2">
-                    <label className="w-1/2 inline-block text-secondary text-sm font-bold mb-2" htmlFor="upload">
-                        <div className="flex justify-center items-center flex-col h-60 w-80 border-2 border-dashed rounded-lg">
+            <div className="md:flex md:flex-wrap w-full mb-2 p-2">
+                {
+                    photos.map((photo, index) => {
+                        return (
+                            <div key={photo} className="md:w-1/2 text-secondary text-sm font-bold mb-2 p-2 ">
+                                <div className="border-2 relative border-dashed overflow-hidden rounded-lg h-80 md:h-60 lg:h-80">
+                                    <img src={photo} className="w-full" style={{ height: "fit-content" }} />
+                                    <button
+                                        onClick={() => handleDeleteImg(index)}
+                                        className="absolute top-0 right-0 rounded w-10 h-10 bg-gray-200 flex justify-center items-center text-xl">
+                                        <FaTimes />
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                <div className="md:w-1/2 block text-secondary text-sm font-bold mb-2 p-2">
+                    <label className="cursor-pointer" htmlFor="upload">
+                        <div className="flex justify-center items-center flex-col  h-80 md:h-60 lg:h-80 w-full border-2 border-dashed rounded-lg">
                             <FaCloudUploadAlt className="text-7xl" />
                             <p className="px-6 text-sm">Choose an image</p>
                             <p className="px-6 text-sm">JPG, PNG, GIF, Max</p>
@@ -71,21 +74,14 @@ const Photos = ({ steps, setSteps }) => {
                             onChange={onSelectFile}
                         />
                     </label>
-                    <div className="w-1/2">
-                        {
-                            photo?.map(img => <div className="w-1/4 inline-block mb-2">
-                                <img className="w-20 h-20 block m-auto border-2" src={img} alt="" />
-                            </div>)
-                        }
-                    </div>
                 </div>
+            </div>
 
-                <p className="my-5 text-sm ml-2">Tip: Choose the top 8-10 photos of your home from different angles in good light that really show the space.</p>
+            <p className="my-5 text-sm ml-2">Tip: Choose the top 8-10 photos of your home from different angles in good light that really show the space.</p>
 
-                <div className="w-full mb-2 p-2">
-                    <button type="submit" className=" bg-green-400 text-white rounded py-2 px-12">Publish</button>
-                </div>
-            </form>
+            <div className="w-full mb-2 p-2">
+                <button type="submit" className=" bg-green-400 text-white rounded py-2 px-12">Publish</button>
+            </div>
         </div>
     );
 };
