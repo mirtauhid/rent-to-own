7e81d0518d6b2d89b7470150e4631a679675f791
Tanvir Islam <9263490-tanvir917@users.noreply.gitlab.com>
2021-09-22 00:49:36 +0600

    homepage links, house filter, url query

--- Components/SubPage/HouseSearch/Interior/index.js
+++ Components/SubPage/HouseSearch/Interior/index.js
@@ -3,32 +3,43 @@ import React from 'react'
 const index = () => {
     return (
         <div>
-            <div className="mt-5">
-                <h1 className="text-xl text-gray-400 font-bold">Interior features</h1>
-                <div className="mt-4 flex justify-between">
-                    <p className="text-md">Entire home</p>
-                    <p className="text-md ml-4">Entire home</p>
-                    <p className="text-md ml-4">Entire home</p>
+            <h1 className="text-xl text-gray-400 font-bold">Interior features</h1>
+            <div className="mt-5 grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3">
+                
+                <div className="">
+                    <p className="text-md">Range</p>
+                    <p className="text-md py-4">Wood cook stove</p>
+                    
                 </div>
-                <div className="mt-4 flex justify-between">
-                    <p className="text-md">Entire home</p>
-                    <p className="text-md ml-4">Entire home</p>
-                    <p className="text-md ml-4">Entire home</p>
+                <div className="">
+                    <p className="text-md">Deep soaker tub</p>
+                    <p className="text-md py-4">Open upper floor loft</p>
+                    
+                </div>
+                <div className="">
+                    <p className="text-md">Propane range</p>
+                    <p className="text-md py-4">custom log finishes</p>
                 </div>
             </div>
             <hr className="mt-5"></hr>
             {/* Exterior features */}
-            <div className="mt-5">
-                <h1 className="text-xl text-gray-400 font-bold">Exterior features</h1>
-                <div className="mt-4 flex justify-between">
-                    <p className="text-md">Entire home</p>
-                    <p className="text-md ml-4">Entire home</p>
-                    <p className="text-md ml-4">Entire home</p>
+            <h1 className="text-xl text-gray-400 font-bold">Exterior features</h1>
+            <div className="mt-5 grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3">
+                
+                <div className="pr-2">
+                    <p className="text-md">Deck or patio</p>
+                    <p className="text-md py-4">Workshop</p>
+                    <p className="text-md">Agricultural tools & equipment</p>
                 </div>
-                <div className="mt-4 flex justify-between">
-                    <p className="text-md">Entire home</p>
-                    <p className="text-md ml-4">Entire home</p>
-                    <p className="text-md ml-4">Entire home</p>
+                <div className="pt-4 md:pt-0 pr-2">
+                    <p className="text-md">Sprinkler system</p>
+                    <p className="text-md py-4">Open upper floor loft</p>
+                    <p className="text-md">Custom log finishes</p>
+                </div>
+                <div className="pt-4 md:pt-0">
+                    <p className="text-md">Agricultural tools</p>
+                    <p className="text-md py-4">Large garden area</p>
+                    <p className="text-md">Livestock shelter & fenced area</p>
                 </div>
             </div>
             <hr className="mt-5"></hr>
