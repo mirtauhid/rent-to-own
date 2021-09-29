9071a02d0448f69901e4804b3b3d457c97e2a3c6
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2021-09-29 16:23:14 +0600

    admin login & signup completed

---
+++ pages/dashboard/signup/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SubSignUp from '../../../Components/SubPage/SubDashboard/SubSignUp';
+
+const index = () => {
+    return (
+        <SubSignUp/>
+    );
+};
+
+export default index;
