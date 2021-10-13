ae2ed3280b761c168e8ad415cdaff0c91bbb1498
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2021-10-13 16:38:46 +0600

    modified house searching & single housing

--- Components/Modal/CustomModal.js
+++ Components/Modal/CustomModal.js
@@ -12,7 +12,8 @@ const CustomModal = ({ children, isOpen, customClasses, isMiddle }) => {
             left: 0,
             right: 0,
             bottom: 0,
-            backgroundColor: 'rgba(0, 0, 0, 0.2)'
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+            zIndex: 100
         },
         content: {
             border: 0,
