31b6989b9320cc5a6880521fccf001490f481855
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2021-10-14 15:16:00 +0600

    some changes

--- pages/sellerProfile/yourListings.js
+++ pages/sellerProfile/yourListings.js
@@ -1,25 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import YourListingsPage from '../../Components/SubPage/SellerProfile/YourListingsPage';
 import HomeLayout from '../../Layouts/HomeLayout';
 
 const YourListings = () => {
-    useEffect(() => {
-        const handleTest = () => {
-            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-            (function () {
-                var s1 = document?.createElement("script");
-                var s0 = document?.getElementsByTagName("script")[0];
-                s1.async = true;
-                s1.src = 'https://embed.tawk.to/6167c082f7c0440a591e1c5f/1fhukg0ft';
-                s1.charset = 'UTF - 8';
-                s1.setAttribute('crossorigin', '*');
-                s0.parentNode?.insertBefore(s1, s0);
-            })();
-        }
-        document && handleTest()
-
-    }, [])
-
     return (
         <HomeLayout>
             <YourListingsPage />
